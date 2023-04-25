57cf065488d7ff2d6eef0e84724aa1a0dca94486
João Pedro <joaopedrodamaiamoura@outlook.com>
2023-04-24 23:09:01 -0300

    Feat: Pequenas mudanças no conteúdo textual do header

--- FrontEnd/src/components/Header/index.js
+++ FrontEnd/src/components/Header/index.js
@@ -9,14 +9,14 @@ export default function Header() {
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="./" className="m-auto md:m-0 flex items-center">
+              <span className="self-center text-xl font-semibold whitespace-nowrap mx-2 dark:text-white">
+                Tech Records
+              </span>
               <img
                 src="./img/music.png"
                 className="mr-3 h-6 sm:h-9"
                 alt="Tech Records"
               />
-              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                Tech Records
-              </span>
             </a>
 
             <div
@@ -30,7 +30,7 @@ export default function Header() {
                     className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
                     aria-current="page"
                   >
-                    Cadastre-se
+                    Inicio
                   </a>
                 </li>
                 <li>
@@ -65,7 +65,7 @@ export default function Header() {
                 href="./"
                 className="text-white bg-blue-800 hover:bg-blue-900 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none dark:focus:ring-primary-800"
               >
-                Cadastre-se
+                Login
               </button>
               {/* botao mobile */}
               <button

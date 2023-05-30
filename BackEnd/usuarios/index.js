afbebd50601768e84135641dda5410702001b57e
Caio Ryan <ccaio4567@gmail.com>
2023-05-29 21:58:50 -0300

    Feat: Get e Get por ID feitos/ problemas com post

--- BackEnd/usuarios/index.js
+++ BackEnd/usuarios/index.js
@@ -3,6 +3,8 @@ const app = express();
 app.use(express.json());
 const axios = require('axios');
 require('dotenv').config()
+const cors = require('cors')
+const mysql = require('mysql2')
 const {DB_USER,DB_HOST,DB_DATABASE,DB_PASSWORD}  = process.env
 
 
@@ -19,10 +21,10 @@ const { v4: uuidv4 } = require('uuid');
 const usuarios = {};
 
 const connection = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  database: DB_DATABASE,
-  password: DB_PASSWORD
+  host: "localhost",
+  user: "root",
+  database: "db_gravadora",
+  password: "Ethyamet@12"
 })
 
 app.get ('/usuarios', (req, res) => {
@@ -35,10 +37,13 @@ app.get ('/usuarios', (req, res) => {
   })
 });
 
-app.get('/usuarios/:id', (req, res) => {
-  connection.query("select * from tb_usuario where idUsuario = ?", (err, result) => {
+app.get('/usuarios/:idUsuario', (req, res) => {
+
+const userId = req.params.idUsuario
+
+  connection.query(`select * from tb_usuario where idUsuario =${userId}`, (err, result) => {
     if(err){
-      console.log("Erro:", err)
+      console.log(err)
     }else{
       res.json(result)
     }
@@ -47,31 +52,66 @@ app.get('/usuarios/:id', (req, res) => {
 
 app.post('/usuarios', async (req, res) => {
   // const idUsuario = uuidv4();
-  // const infosUsuario = {...req.body, idUsuario};
+  const nome = req.body.nomeCompleto
+  const genero = req.body.genero
+  const data = req.body.dataDeNascimento
+  const email = req.body.email
+  const cep = req.body.cep
+  const portifolio = req.body.linksPortifolio
+  const descricao = req.body.descricao
+  const habilidades = req.body.habilidades
+  const banda = req.body.banda
+  const termoDeUso = req.body.termoDeUso
+
   // console.log(req.body);
   // usuarios[idUsuario] = {
   //   ...infosUsuario
   // }
 
-  connection.query("")
+  const sql = "insert into tb_usuario(nomeCompleto, genero, dataDeNascimento, email, cep, linkPortifolio, descricao, banda, habilidades, termoDeUso) values (?,?,?,?,?,?,?,?,?,?)"
+
+  connection.query(sql,[nome, genero, data, email, cep, portifolio, descricao, banda, habilidades, termoDeUso],(err,result) => {
+    if(err){
+      res.send(err)
+    }else{
+      res.status(201).send("Usuário cadastrado com sucesso!", result)
+    }
+  })
 
   await axios.post('http://localhost:10000/eventos', {
     tipo: "UsuarioCriado",
     dados: {
-      ...infosUsuario
+      nome, genero, data, email, cep, portifolio, descricao, banda, habilidades, termoDeUso
+    }
+  })
+});
+
+app.put('/usuarios/:idUsuario', async(req,res) => {
+  
+  const idUsuario = req.params.idUsuario
+
+  const nome = req.body.nomeCompleto
+  const genero = req.body.genero
+  const data = req.body.dataDeNascimento
+  const email = req.body.email
+  const cep = req.body.cep
+  const portifolio = req.body.linksPortifolio
+  const descricao = req.body.descricao
+  const habilidades = req.body.habilidades
+  const banda = req.body.banda
+
+  connection.query(`update tb_usuario set nomeCompleto=${nome}, genero=${genero}, dataDeNascimento=${data}, email=${email}, cep=${cep}, linkPortifolio=${portifolio}, descricao=${descricao}, habilidades=${habilidades}, banda=${banda} where idUsuario = ${idUsuario}`,(err, result) =>{
+    if(err){
+      console.log("Erro:", err)
+    }else{
+      console.log("Usuário atualizado com suceeso!", result)
     }
   })
 
-  res.status(201).send(usuarios[idUsuario]);
-});
+  // const infosUsuario = {...req.body, idUsuario}
 
-app.put('/usuarios/:id', async(req,res) => {
+  // usuarios[idUsuario] = infosUsuario
   
-  const idUsuario = req.params.id
-
-  const infosUsuario = {...req.body, idUsuario}
-
-  usuarios[idUsuario] = infosUsuario
   res.send("Dados atualizados com sucesso!")
 
   await axios.post('http://localhost:10000/eventos', {
@@ -82,8 +122,8 @@ app.put('/usuarios/:id', async(req,res) => {
   })
 })
 
-app.delete('/usuarios/:id', (req, res) => {
-  const userID = req.params.id
+app.delete('/usuarios/:idUsuario', (req, res) => {
+  const userID = req.params.idUsuario
   
   connection.query(`delete from tb_usuario where idUsuario = ${userID}`,(err, result) => {
     if(err){
